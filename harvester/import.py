24fdd8e391d4c281289e434af2fb6c4e23fc2611
alanchenyx <alanchenyixin@gmail.com>
2017-05-01 01:07:56 +1000

    Import twitterfile to couchdb
    Deal with duplicates, catch exception - ResourceConflict

--- harvester/import.py
+++ harvester/import.py
@@ -1,11 +1,12 @@
 import couchdb
+from couchdb import ResourceConflict
 import json
 #from harvester.service import TweetHarvestingService as tweet_service
 
 ausgrid = [111.89575195844913,-44.01059552028073,154.9154663073704,-9.93368223492055]
-couch = couchdb.Server('serverlink')
-db = couch['dbname']
-COUNT = 0
+couch = couchdb.Server('couchdb_host')
+db = couch['couchdb_database']
+#COUNT = 0
 
 def checklocation(coordinate):
     if coordinate[0] > ausgrid[0] and coordinate[0] < ausgrid[2] and \
@@ -15,19 +16,35 @@ def checklocation(coordinate):
         return False
 
 def twitterprocess(raw_data):
-    coordinate = data = (json.loads(raw_data)['json']['coordinates']['coordinates'])
+    coordinate =  (json.loads(raw_data)['json']['coordinates']['coordinates'])
     if checklocation(coordinate):
         data = (json.loads(raw_data)['json'])
-        data['_id'] = data['id_str']
-        db.save(data)
-        print(data['_id'])
-        global COUNT
-        COUNT = COUNT + 1
+        skip = False
+        try:
+            data['_id'] = data['id_str']
+        except KeyError:
+            try:
+                data['_id'] = data['id']
+            except KeyError as e:
+                msg = '[SKIP] Tweet without id_str or id.\n'
+                msg += json.dumps(data)
+                skip = True
+        if skip:
+            pass
+        else:
+            data['_id'] = data['id_str']
+            try:
+                db.save(data)
+                #global COUNT
+                #COUNT = COUNT + 1
+            except ResourceConflict:
+                pass
+
     return
 
 def readtwitter():
     print('Start reading twitter file...')
-    with open('tinyTwitter.json', 'rt', encoding='latin1') as tweet_data:
+    with open('bigTwitter.json', 'rt', encoding='latin1') as tweet_data:
         for line in tweet_data:
             if line[0] == '{':
                 data = ''
@@ -36,7 +53,7 @@ def readtwitter():
                     twitterprocess(line[:-1])
                 else:
                     twitterprocess(line)
-        print('Toal number of tweets: ' + str(COUNT))
+        #print('Toal number of tweets: ' + str(COUNT))
     return
 
 readtwitter()
