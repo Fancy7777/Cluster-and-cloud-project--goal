37d7388572dc1902ed01bcdb277a46b3cd0dd06b
alanchenyx <alanchenyixin@gmail.com>
2017-05-09 02:22:43 +1000

    update

--- analysis/aurin_importer.py
+++ analysis/aurin_importer.py
@@ -1,16 +1,25 @@
 import couchdb
 import json
+from harvester import tweepyquery_tag as config
 
-#modify it before import
-couch = couchdb.Server('address')
-db = couch['aurin_dataset_number']
+couch = couchdb.Server(config.connection)
 
 try:
     filename = input("Give the name of the file you wish to import to couchdb and make sure it's in the same folder:")
 except ValueError:
     print("not a valid input")
 
-with open('filename') as json_data:
+with open(filename) as json_data:
     data = json.load(json_data)
 
-db.save(data)
+try:
+    tweet_db_name = input("Importing to which db?:")
+except ValueError:
+    print("not a valid input")
+
+try:
+    targetdb = couch[tweet_db_name]
+except couchdb.http.ResourceNotFound:
+    targetdb = couch.create(tweet_db_name)
+
+targetdb.save(data)
