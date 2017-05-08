e068cc579ab738f868507063b2d557d4946bcf22
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2017-05-08 23:53:37 +1000

    pie chart drawn, bar chart drawn..

--- webapp/assets/js/barchart.jsx
+++ webapp/assets/js/barchart.jsx
@@ -18,10 +18,21 @@ class BarChart extends React.Component {
 			mode: 'cors',
 			cache: 'default'
 		}
+		this.aurinDataBachelor = {
+			"sydney": 43040,
+			"melbourne": 25532,
+			"adelaide": 4490,
+			"brisbane": 173233,
+			"canberra": 61827,
+			"darwin": 8755,
+			"hobart": 9901,
+			"perth": 4199
+		}
 		this.plain = this.plain.bind(this)
 		this.inverted = this.inverted.bind(this)
 		this.polar = this.polar.bind(this)
 		this.mapReduceMajorCityView = "http://115.146.94.41:5000/tweet_raw_trump/_design/trump_by_major_city/_view/trump_by_major_city"
+		this.updateChart = this.updateChart.bind(this)
 	}
 
 	fetchJsonData(url) {
@@ -36,7 +47,7 @@ class BarChart extends React.Component {
 				        type: 'column'
 				    },
 				    title: {
-				        text: "Sentimen Analysis of people's attitudes for Trump"
+				        text: "Sentimen Analysis of people's postive and negative attitude rates for Trump vs bachelor rate of each Australia's major city"
 				    },
 				    subtitle: {
 				        text: 'Source: www.twitter.com'
@@ -47,18 +58,19 @@ class BarChart extends React.Component {
 						// 	"Positive", "Negative", "Compound", "Neutral"
 				        // ],
 						categories: majorCityData["categories"],
+
 				        crosshair: true
 				    },
 				    yAxis: {
 				        min: 0,
 				        title: {
-				            text: 'number'
+				            text: 'rate'
 				        }
 				    },
 				    tooltip: {
 				        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
 				        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-				            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>', // can add unit after {point.y:.1f}
+				            '<td style="padding:0"><b>{point.y:.3f}</b></td></tr>', // can add unit after {point.y:.1f}
 				        footerFormat: '</table>',
 				        shared: true,
 				        useHTML: true
@@ -90,27 +102,78 @@ class BarChart extends React.Component {
 		let processedData = {}
 
 		for(let i = 0; i < cityKeySet.length; i++) {
-			processedData[cityKeySet[i]] = []
+				processedData[cityKeySet[i]] = {}
+
 		}
 
 		for (let i = 0; i < cityKeySet.length; i++) {
 			for (let j = 0; j < sentimentTagCategories.length; j++) {
-				processedData[cityKeySet[i]].push(values[cityKeySet[i]][sentimentTagCategories[j]])
+				processedData[cityKeySet[i]][sentimentTagCategories[j]] =values[cityKeySet[i]][sentimentTagCategories[j]]
 			}
 		}
 
-
-
 		let dataSeries = []
+		let sumSentiment = {"pos":0, "neg": 0}
 
+		let aurinDataBachelorSum = 0
+
+
+		for (let i = 0; i < cityKeySet.length; i++) {
+			aurinDataBachelorSum += this.aurinDataBachelor[cityKeySet[i]]
+			console.log("aurinDataBachelorSum => " + aurinDataBachelorSum)
+		}
+
+		let aurinDataBachelorInGraph = []
 
 		for(let i = 0; i < cityKeySet.length; i++) {
-			dataSeries.push({"name": cityKeySet[i], "data": processedData[cityKeySet[i]]})
+			console.log("bachelor rate => " + this.aurinDataBachelor[cityKeySet[i]]/aurinDataBachelorSum)
+			aurinDataBachelorInGraph.push(this.aurinDataBachelor[cityKeySet[i]]/aurinDataBachelorSum)
 		}
-		console.log(" dataSeries name => " + dataSeries[0]["name"])
-		console.log(" dataSeries data => " + dataSeries[0]["data"])
 
-		return {"categories": sentimentTagCategories, "dataSeries":dataSeries}
+		dataSeries.push({"type":"column", "name": "bachelor", "data": aurinDataBachelorInGraph, colorIndex:5})
+
+
+		for(let i = 0; i < sentimentTagCategories.length; i++) {
+			if (sentimentTagCategories[i] != "pos") {
+				if (sentimentTagCategories[i] != "neg") {
+					console.log(" pass => " + sentimentTagCategories[i])
+					continue
+				}
+			}
+			let tempSentimentDataArray = []
+			for(let j = 0; j < cityKeySet.length; j++) {
+				tempSentimentDataArray.push(processedData[cityKeySet[j]][sentimentTagCategories[i]])
+			}
+
+			sumSentiment[sentimentTagCategories[i]] = tempSentimentDataArray.reduce((a, b) => a + b, 0)
+			tempSentimentDataArray = tempSentimentDataArray.map((a) => { return (a/sumSentiment[sentimentTagCategories[i]]) })
+			dataSeries.push({"type":"column", "name": sentimentTagCategories[i], "data": tempSentimentDataArray, colorIndex:i})
+		}
+		// console.log(" dataSeries name => " + dataSeries[0]["name"])
+		// console.log(" dataSeries data => " + dataSeries[0]["data"])
+
+		// let pointStart = [ -0.2, 0, 0]
+		//
+		// for(let i = 0; i < sentimentTagCategories.length; i++) {
+		// 	let template = {
+		// 		type: 'spline',
+		// 		name: 'Average',
+		// 		data: dataSeries[i]["data"],
+		// 		dashStyle: "Solid",
+		// 		pointStart: pointStart[0],
+		// 		colorIndex: i,
+		// 		marker: {
+		// 			lineWidth: 2,
+		// 			lineColor: "black",
+		// 			fillColor: 'white',
+		// 			radius: 2,
+		// 		}
+		// 	}
+		//
+		// 	dataSeries.push(template)
+
+		// }
+		return {"categories": cityKeySet, "dataSeries":dataSeries}
 	}
 
 
@@ -156,11 +219,11 @@ class BarChart extends React.Component {
 	}
 
 
-	updateChart()
-	{
-		if (this.chart != null)
-		{
-			this.chart.update(this.props.chartData, this.options, false)
+	updateChart() {
+		if (this.chart != null) {
+			console.log("reflow")
+			this.chart.reflow()
+			this.chart.redraw()
 		}
 
 	}
