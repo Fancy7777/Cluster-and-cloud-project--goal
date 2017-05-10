607ee63cd98c7a0a74810de090461c9b6f9a1f47
liubingfeng93@gmail.com <liubingfeng93@gmail.com>
2017-05-10 22:48:15 +1000

    heat map can be drawn now

--- webapp/assets/js/heatmapbox.jsx
+++ webapp/assets/js/heatmapbox.jsx
@@ -10,7 +10,10 @@ class HeatMapBox extends React.Component{
 
 	constructor(props) {
     	super(props)
-		this.bbox = {}
+		// darwin and hobart has no bbox
+		// darwin center => [-78.848131, 42.936161]
+		// hobart center => [147.510278, -42.836112]
+		this.bbox = {"darwin": [130.57877992453348, -13.061934330979502, 131.58402894780482, -12.334576047114853] , "hobart": [147.0088336325362, -43.14103367624911, 147.6515338277326, -42.70553445868015]}
 		this.myHeaders = new Headers({
 			  "Authorization": "Basic " + btoa("cuser" + ":" + "JumpyMonk3y"),
 			})
@@ -42,40 +45,68 @@ class HeatMapBox extends React.Component{
 		})
 	}
 
+	sleep (time) {
+  		return new Promise((resolve) => setTimeout(resolve, time));
+	}
+
 	componentDidMount() {
-		// this.fetchBbox("sydney")
-		// Mel
-		// let mymap = L.map(this.props.mapid).setView([50.5, 30.5], 12)
-		let mymap = L.map(this.props.mapid).setView([-37.7963689,144.9589851], 10)
-		L.tileLayer(this.props.mapboxAccessUrl, {
-			maxZoom: 18,
-		}).addTo(mymap)
 
-		L.geoJson(melbourneGeo).addTo(mymap)
-
-		let points = addressPoints.map((p) => { return [p[0], p[1]] })
-		// console.log("points => " + points[0] );
-		// console.log("L.heatLayer => " + L.heatLayer );
-		let heat = L.heatLayer(points, {minOpacity: 0.1}).addTo(mymap);
-		// console.log("heat => " + heat);
 
 
 		fetch(this.cityAboveScoresCountURL, this.fetchOptions).
 		then( (response) => { return response.json() } ).
 		then( (json) => {
+
 			console.log("city score count => " + json["rows"][0]["value"]["adelaide"])
 
 			let cityAboveScoresCountResult = json["rows"][0]["value"]
-			let cityNames = Object.keys(["rows"][0]["value"])
+			let cityNames = Object.keys(json["rows"][0]["value"])
 			console.log("cityAboveScoresCountResult keys => " + json["rows"][0]["value"]["adelaide"])
 
-			for(let i = 0; i < cityAboveScoresCountResult.length; i++) {
-				let tempScore = cityAboveScoresCountResult[cityNames[i]]
-				tempScore = parseInt(tempScore)
-				for(let j = 0; j < tempScore; j++){
-					
-				}
+			for(let i = 0; i < cityNames.length; i++) {
+				this.fetchBbox(cityNames[i])
 			}
+
+
+			this.sleep(2000).then(() => {
+
+				// this.fetchBbox("sydney")
+				// Mel
+				// let mymap = L.map(this.props.mapid).setView([50.5, 30.5], 12)
+				let mymap = L.map(this.props.mapid).setView([-37.7963689,144.9589851], 5)
+				L.tileLayer(this.props.mapboxAccessUrl, {
+					maxZoom: 18,
+				}).addTo(mymap)
+
+				// L.geoJson(melbourneGeo).addTo(mymap)
+
+				let points = addressPoints.map((p) => { return [p[0], p[1]] })
+				// console.log("points => " + points[0] );
+				// console.log("L.heatLayer => " + L.heatLayer );
+				let heat = L.heatLayer([], {minOpacity: 0.5, radius: 10}).addTo(mymap);
+				// console.log("heat => " + heat);
+
+				// Do something after the sleep!
+				console.log("wait for 1 s finished")
+				for(let i = 0; i < cityNames.length; i++) {
+					let tempScore = cityAboveScoresCountResult[cityNames[i]]
+					tempScore = parseInt(tempScore)
+					let tempBbox = this.bbox[cityNames[i]]
+					let minY = this.bbox[cityNames[i]][0]
+					let minX = this.bbox[cityNames[i]][1]
+					let maxY = this.bbox[cityNames[i]][2]
+					let maxX = this.bbox[cityNames[i]][3]
+					console.log (" get right bbox => " + tempBbox + " city name => " + cityNames[i])
+					for(let j = 0; j < tempScore; j++){
+						let longlat = [Math.random()*(maxX-minX)+minX,Math.random()*(maxY-minY)+minY, 1]
+						console.log("heap map lat lon =>" + longlat)
+						heat.addLatLng(longlat)
+					}
+				}
+			})
+
+
+
 
 		} )
 
